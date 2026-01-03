309c57c41a23465758103431af62a71a167f0360
tac1DEV <sansberrot@gmail.com>
2026-01-03 16:23:51 +0100

    modification du model User et de son fichier de migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,10 +12,10 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('name');
             $table->string('password');
             $table->string('email')->unique();
-            $table->string('role');
+            $table->string('role')->default('client');
             $table->timestamps();
         });
     }
