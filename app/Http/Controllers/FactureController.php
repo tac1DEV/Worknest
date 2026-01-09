ffe828612927ac1862d8cad44bbd1d81d71cd81f
tac1DEV <sansberrot@gmail.com>
2026-01-09 17:59:41 +0100

    feat: Ajout de la facture d'une reservation

---
+++ app/Http/Controllers/FactureController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use LaravelDaily\Invoices\Invoice;
+use LaravelDaily\Invoices\Classes\Party;
+use LaravelDaily\Invoices\Classes\InvoiceItem;
+use App\Models\Schedule;
+use Carbon\Carbon;
+
+class FactureController extends Controller
+{
+    public function show($scheduleId = 2)
+    {
+        $schedule = Schedule::findOrFail($scheduleId);
+        $siren = fake()->siren();
+        $numero_de_commande = rand(1000, 65000);
+        $code = rand(10000000, 99000000);
+        $datesDebut = Carbon::parse($schedule->start)->format('d/m/Y');
+        $datesFin = Carbon::parse($schedule->end)->format('d/m/Y');
+        $start = Carbon::parse($schedule->start)->startOfDay();
+        $end = Carbon::parse($schedule->end)->startOfDay();
+        $days = $start->diffInDays($end);
+
+        $seller = new Party([
+            'name' => 'Worknest',
+            'phone' => '01 23 45 67 89',
+            'custom_fields' => [
+                'Siren' => $siren,
+            ],
+        ]);
+
+        $customer = new Party([
+            'name' => $schedule->user->name,
+            'address' => '1 rue de la liberté',
+            'code' => '#' . $code,
+            'custom_fields' => [
+                'Numero de commande' => $numero_de_commande,
+            ],
+        ]);
+
+        $items = [
+            InvoiceItem::make('Réservation du ' . $datesDebut . ' à ' . $datesFin . '.')
+                ->description($schedule->title . ' - Espace ' . $schedule->espace->nom)
+                ->pricePerUnit($schedule->espace->categorie->prix)
+                ->quantity($days),
+        ];
+
+        $invoice = Invoice::make()
+            ->series('BIG')
+            ->status(__('invoices::invoice.paid'))
+            ->sequence(667)
+            ->serialNumberFormat('{SEQUENCE}/{SERIES}')
+            ->seller($seller)
+            ->buyer($customer)
+            ->date(Carbon::now())
+            ->dateFormat('d/m/Y')
+            ->payUntilDays(14)
+            ->currencySymbol('€')
+            ->currencyCode('EUR')
+            ->currencyFormat('{SYMBOL}{VALUE}')
+            ->currencyThousandsSeparator('.')
+            ->currencyDecimalPoint(',')
+            ->filename($seller->name . ' ' . $customer->name)
+            ->addItems($items)
+            ->logo(public_path('vendor/invoices/sample-logo.png'))
+            // You can additionally save generated invoice to configured disk
+            ->save('public');
+
+        $link = $invoice->url();
+        // Then send email to party with link
+
+        // And return invoice itself to browser or have a different view
+        return $invoice->stream();
+    }
+    public function calculerPrix($schedule)
+    {
+        // Calculer la durée en jours
+        $start = Carbon::parse($schedule->start)->startOfDay();
+        $end = Carbon::parse($schedule->end)->startOfDay();
+        $days = $start->diffInDays($end);
+
+        // Prix basé sur la catégorie de l'espace
+        $prixParJour = $schedule->espace->categorie->prix;
+        $total = $days * $prixParJour;
+
+        return $total;
+    }
+}
--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -18,9 +18,10 @@ class ScheduleFactory extends Factory
     {
         $dateDebut = now()->addSeconds(random_int(1, 604800));
         $color = ['#ff0000', '#00ff00', '#0000ff'];
+        $title = ['Meeting', 'Conférence', 'Réunion du comité', 'Brainstorming'];
 
         return [
-            'title' => $this->faker->name(),
+            'title' => $title[array_rand($title)],
             'start' => $dateDebut,
             'end' => $dateDebut->copy()->addHour(),
             'color' => $color[array_rand($color)],
