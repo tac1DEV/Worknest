7c66cba4b0ef44c9333e643fb3ce0c47b9f415ec
tac1DEV <sansberrot@gmail.com>
2026-01-10 20:03:17 +0100

    bug:it_can_filter_espaces_by_capacity

--- tests/Feature/EspaceTest.php
+++ tests/Feature/EspaceTest.php
@@ -5,16 +5,43 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Espace;
+use PHPUnit\Framework\Attributes\Test;
+
 
 class EspaceTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    #[Test]
+    public function it_displays_list_of_espaces()
+    {
+        $espace = Espace::factory()->create();
+        $response = $this->get('/espaces');
         $response->assertStatus(200);
+        $response->assertSee($espace->name);
     }
+
+    #[Test]
+    public function it_displays_espace_details_in_list()
+    {
+        $espace = Espace::factory()->create();
+        $response = $this->get('/espaces');
+        $response->assertSee($espace->name)
+            ->assertSee((string) $espace->capacity)
+            ->assertSee((string) $espace->price);
+    }
+
+    #[Test]
+    public function it_can_filter_espaces_by_capacity()
+    {
+        $small = Espace::factory()->create(['capacite' => 50]);
+        $medium = Espace::factory()->create(['capacite' => 75]);
+        $large = Espace::factory()->create(['capacite' => 100]);
+        $response = $this->get('/espaces?filter[capacite]=100');
+        $response->assertViewHas('espaces', function ($espaces) use ($large) {
+            return $espaces->first()->id === $large->id;
+        });
+    }
+
 }
